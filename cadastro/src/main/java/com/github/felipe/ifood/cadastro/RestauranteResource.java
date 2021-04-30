b904cc6b82ec6f4f7bdfdc3360b3604f9ce2f1be
Felipe da Rosa <felipe.rosa@externo.cresol.coop.br>
2021-04-30 13:04:14 -0300

    Adicionando autorização com keycloak e tracing com jaeger ao projeto

--- cadastro/src/main/java/com/github/felipe/ifood/cadastro/RestauranteResource.java
+++ cadastro/src/main/java/com/github/felipe/ifood/cadastro/RestauranteResource.java
@@ -2,10 +2,15 @@ package com.github.felipe.ifood.cadastro;
 
 import com.github.felipe.ifood.cadastro.common.ConstraintViolationResponse;
 import com.github.felipe.ifood.cadastro.dto.*;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -22,6 +27,9 @@ import static javax.ws.rs.core.Response.Status.CREATED;
 @Path("/restaurantes")
 @Produces(APPLICATION_JSON)
 @Consumes(APPLICATION_JSON)
+@RolesAllowed("proprietario")
+@SecurityScheme(securitySchemeName = "ifood-oauth", type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/ifood/protocol/openid-connect/token")))
 public class RestauranteResource {
 
     @Inject
